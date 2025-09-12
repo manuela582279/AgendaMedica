a636af1526298ebd8d9008827992f18cc499d15d
mManuela582279 <manuela.domingues@aluno.senai.br>
2025-09-12 09:21:20 -0300

    Adiciona CRUD e migrações para novas entidades
    Foram adicionadas as entidades `Especialidade`, `Medico`, `Paciente` e `Agenda` ao modelo de dados, com suas respectivas tabelas e relacionamentos configurados no `ApplicationDbContext`. Controladores e páginas Razor foram criados para gerenciar as operações CRUD dessas entidades, incluindo validações e tratamento de erros.
    
    O layout foi atualizado para incluir links de navegação para as novas funcionalidades, e as rotas dos controladores foram protegidas com `[Authorize]` para garantir acesso apenas a usuários autenticados. Migrações foram criadas para refletir as alterações no banco de dados, incluindo a criação de tabelas e ajustes no modelo de `Paciente`.
    
    Essas mudanças introduzem funcionalidades essenciais para o gerenciamento de especialidades, médicos, pacientes e agendas de consulta no sistema.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AgendaMedica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AgendaMedica.Data
@@ -6,12 +7,18 @@ namespace AgendaMedica.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
+        public DbSet<Especialidade> especialidades { get; set; }
+        public DbSet<Medico> medicos { get; set; }
+        public DbSet<Paciente> pacientes { get; set; }
+        public DbSet<Agenda> agendas { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<Especialidade>().ToTable("Especialidades");
+            builder.Entity<Medico>().ToTable("Medicos");
+            builder.Entity<Paciente>().ToTable("Pacientes");
+            builder.Entity<Agenda>().ToTable("Agendas");
         }
     }
 }
---
+++ Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaMedica.Models
+{
+    public class Agenda
+    {
+        public Guid AgendaId { get; set; }
+        public Guid PacienteId { get; set; }
+        public Paciente? Paciente { get; set; }
+        public Guid MedicoId { get; set; }
+        public Medico? Medico { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string Status { get; set; }
+    }
+}
